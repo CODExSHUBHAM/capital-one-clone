bd60b17bab7081e395bea7b2a773915ada510364
SHUBHAM KASHYAP <98178320+CODExSHUBHAM@users.noreply.github.com>
2023-10-06 00:49:35 +0530

    Update home.jsx

--- frontend/src/components/Home/home.jsx
+++ frontend/src/components/Home/home.jsx
@@ -10,7 +10,7 @@ const Home = () => {
   const [catagories, setCatagories] = useState([]);
   const [deals, setDeals] = useState([]);
 
-  useEffect(() => {
+  // useEffect(() => {
 
     const getDeals = () => {
 
@@ -25,8 +25,10 @@ const Home = () => {
       
     }
 
-    return () => getDeals();
-  }, [])
+    getDeals()
+
+  //   return () => getDeals();
+  // }, [])
 
   const topDeals = deals.slice(0, 8);
 
