ed8c2a6f3e93abe9e20361c818bdcc931c60bd8e
CODExSHUBHAM <shubham.nh1@gmail.com>
2023-10-06 21:45:04 +0530

    sg

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -24,7 +24,7 @@ const router = createBrowserRouter([
       },
       {
         path: "deals",
-        element: "www.google.com"
+        element: <Deals/>
       },
       {
         path: "catagories",
