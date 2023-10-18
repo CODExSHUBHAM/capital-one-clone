72e2ae7eb9c9c1ec7399f7d8c08749aef81f762a
CODExSHUBHAM <shubham.nh1@gmail.com>
2023-10-18 15:40:37 +0530

    .

--- frontend/src/components/Navbar/navbar.jsx
+++ frontend/src/components/Navbar/navbar.jsx
@@ -9,14 +9,21 @@ import Search from '../Search/search'
 const Navbar = () => {
 
   const [open, setOpen] = useState(false);
+  const [openSearch, setOpenSearch] = useState(false);
 
   const toggleMenu = () => {
     setOpen((prev) => !prev)
+  }
+
+  const toggleSearch = () => {
+    setOpenSearch((prev) => !prev)
+    setOpen(false)
     console.log(open)
   }
 
   const resetMenu = () => {
     setOpen(false)
+    setOpenSearch(false)
   }
 
   return (
@@ -24,37 +31,33 @@ const Navbar = () => {
       {/* Desktop Nav */}
       <div className='border-b bg-white hidden md:flex'>
         <div className='md:flex items-center justify-between py-4 w-[74%] m-auto bg-white hidden'>
-          <Link to='/'><img src={logo} alt="logo" className='w-[190px]' /></Link>
+          <Link to='/' onClick={resetMenu}><img src={logo} alt="logo" className='w-[190px]' /></Link>
           <nav className='text-base font-optimistr'>
             <ul className='flex space-x-8'>
-              <NavLink to='/deals' className='hover:text-blue-600' >Deals</NavLink>
-              <NavLink to='/catagories' className='hover:text-blue-600'>Categories</NavLink>
-              <NavLink to='/resources' className='hover:text-blue-600'>Resources</NavLink>
+              <NavLink to='/deals' onClick={resetMenu} className='hover:text-blue-600' >Deals</NavLink>
+              <NavLink to='/catagories' onClick={resetMenu} className='hover:text-blue-600'>Categories</NavLink>
+              <NavLink to='/resources' onClick={resetMenu} className='hover:text-blue-600'>Resources</NavLink>
             </ul>
           </nav>
           <div className='flex space-x-5 items-center text-base font-optimistr'>
-            <span onClick={toggleMenu}>
+            <span onClick={toggleSearch}>
               <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' className='w-[24px]' fill='%23141414'>
                 <path
                   d='M21.466,19l-5.649-5.466a7.533,7.533,0,1,0-2.508,2.423l5.745,5.557A1.742,1.742,0,0,0,21.466,19ZM3,9.505a6.5,6.5,0,1,1,6.5,6.5A6.51,6.51,0,0,1,3,9.505ZM20.793,20.772a.733.733,0,0,1-.518.226.887.887,0,0,1-.528-.2l-5.6-5.413a7.608,7.608,0,0,0,1.068-1.032l5.554,5.373a.736.736,0,0,1,.227.52A.743.743,0,0,1,20.793,20.772Z' />
               </svg>
             </span>
-            <NavLink className='flex items-center' to='/login'>
+            <NavLink className='flex items-center' to='/login' onClick={resetMenu}>
               <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' className='w-[24px] mr-2' fill='%23141414'>
                 <path
                   d='M12,13A5.5,5.5,0,1,0,6.5,7.5,5.507,5.507,0,0,0,12,13ZM12,3A4.5,4.5,0,1,1,7.5,7.5,4.505,4.505,0,0,1,12,3Zm0,11c-4.868,0-8.581,3.7-9,7.159a.746.746,0,0,0,.186.585A.759.759,0,0,0,3.757,22H20.243a.759.759,0,0,0,.566-.256A.747.747,0,0,0,21,21.158C20.581,17.7,16.868,14,12,14ZM4.039,21C4.564,18.106,7.86,15,12,15s7.437,3.106,7.961,6Z' />
               </svg>
               Sign In
             </NavLink>
-            <NavLink className='bg-[#25810E] px-5 py-1 rounded-sm font-bold text-white' to='/signup'>Join</NavLink>
+            <NavLink className='bg-[#25810E] px-5 py-1 rounded-sm font-bold text-white' to='/signup' onClick={resetMenu}>Join</NavLink>
           </div>
         </div>
       </div>
-      {
-        open ? (
-          <Search />
-        ) : null
-      }
+
 
 
       {/* mobile Nav  */}
@@ -74,7 +77,7 @@ const Navbar = () => {
           </button>
 
           {/* Logo  */}
-          <Link to='/' onClick={() => setOpen(false)}><img src={logo} alt="logo" className='w-[190px]' /></Link>
+          <Link to='/' onClick={resetMenu}><img src={logo} alt="logo" className='w-[190px]' /></Link>
 
           {/* Sign In  */}
           <NavLink onClick={resetMenu} className='flex flex-col items-center justify-center text-xs' to='/signup'>
@@ -91,7 +94,7 @@ const Navbar = () => {
       {open ? (
         <div className='overflow-hidden items-center bg-white absolute w-full h-screen top-18 z-10 p-6 md:hidden text-center space-y-10 text-base font-optimistr'>
           <ul className='flex flex-col text-[#141414]'>
-            <p className='hover:text-blue-600 flex justify-between items-center px-2 py-3 border-b ' >Search <span className='text-2xl font-light'>
+            <p className='hover:text-blue-600 flex justify-between items-center px-2 py-3 border-b ' onClick={toggleSearch} >Search <span className='text-2xl font-light'>
               <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' className='w-[24px]' fill='%23141414'>
                 <path
                   d='M21.466,19l-5.649-5.466a7.533,7.533,0,1,0-2.508,2.423l5.745,5.557A1.742,1.742,0,0,0,21.466,19ZM3,9.505a6.5,6.5,0,1,1,6.5,6.5A6.51,6.51,0,0,1,3,9.505ZM20.793,20.772a.733.733,0,0,1-.518.226.887.887,0,0,1-.528-.2l-5.6-5.413a7.608,7.608,0,0,0,1.068-1.032l5.554,5.373a.736.736,0,0,1,.227.52A.743.743,0,0,1,20.793,20.772Z' />
@@ -114,6 +117,12 @@ const Navbar = () => {
           </div>
         </div>
       ) : null}
+
+      {
+        openSearch ? (
+          <Search />
+        ) : null
+      }
 
     </>
 
--- frontend/src/components/Search/search.jsx
+++ frontend/src/components/Search/search.jsx
@@ -2,8 +2,8 @@
 const search = (props) => {
   return (
     <>
-      <div className={`w-full py-6 bg-white border-b hidden md:flex`}>
-        <div className="w-[74%] m-auto p-3 border border-black rounded-md flex space-x-2">
+      <div className={`w-full py-6 bg-white border-b md:flex`}>
+        <div className="w-[90%] md:w-[74%] m-auto p-3 border border-black rounded-md flex space-x-2">
           <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='%23141414' className="w-[24px]">
             <path
               d='M21.467,19l-5.65-5.466a7.529,7.529,0,1,0-2.508,2.423l5.745,5.556a1.727,1.727,0,0,0,1.2.484H20.3a1.743,1.743,0,0,0,1.171-3ZM3,9.505A6.5,6.5,0,1,1,9.5,16.01,6.51,6.51,0,0,1,3,9.505Z' />
